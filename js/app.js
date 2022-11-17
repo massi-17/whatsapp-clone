485bd1afee91cd0ee73291ac4d879ea063f04dda
massi-17 <massimianisilvia@gmail.com>
2022-11-17 01:01:11 +0100

    funziona solo sui nuovi

--- js/app.js
+++ js/app.js
@@ -4,52 +4,95 @@ let inputText = document.getElementById('inputText')
 
 
 
-let today = new Date();
-inputText.addEventListener("keypress", event => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-    //   chat.innerHTML += `<div class="message">
-    //                         <div>${inputText.value}</div>
-    //                         <span class="time">
-    //                             ${today.getHours()}:${today.getMinutes()}
-    //                             ${icon}
-    //                         </span>
-    //                       </div>  
-    //                      `
+let today = new Date()
 
-const message = document.createElement("div");
-message.classList.add('message')
-chat.appendChild(message)
-let msgText = document.createElement('div')
-msgText.innerHTML = inputText.value
-message.appendChild(msgText)
+let texts = []
 
-let span = document.createElement('span')
-span.classList.add('time')
-message.appendChild(span)
+    
+// texts.push(`${inputText.value[i]}`)
+// localStorage.setItem('text', JSON.stringify(texts));
+
+  
+function stampMsg() {
+  const message = document.createElement("div");
+  message.classList.add('message')
+  chat.appendChild(message)
+  let msgText = document.createElement('div')
+  msgText.innerHTML = inputText.value
+  message.appendChild(msgText)
+
+  let span = document.createElement('span')
+  span.classList.add('time')
+  message.appendChild(span)
 
 
-let date = document.createElement('div')
-date.classList.add('nowTime')
-date.innerHTML = today.getHours() + ':' + today.getMinutes()
+  let date = document.createElement('div')
+  date.classList.add('nowTime')
+  date.innerHTML = today.getHours() + ':' + today.getMinutes()
 
-span.appendChild(date)
+  span.appendChild(date)
 
-let icon = document.createElement("i");
-icon.classList.add('fa')
-icon.classList.add('fa-check-double')
-span.appendChild(icon)
-     
+  let icon = document.createElement("i");
+  icon.classList.add('fa')
+  icon.classList.add('fa-check-double')
+  span.appendChild(icon)
 
-inputText.value = ''
-
-setTimeout(function() {
+ 
+  setTimeout(function() {
     icon.classList.add('read')
-  }, 5000);
+   
+    
+    texts.push(msgText.innerHTML)
+    console.log(texts)
+    localStorage.setItem('text', JSON.stringify(texts));
+  }, 2000)
+}
+
+
+inputText.addEventListener("keypress", event => {
+
+  if (event.key === "Enter") {
+    event.preventDefault()
+    stampMsg()
+    inputText.value = ''
+ 
     }
 })
 
 
+let archiviati = JSON.parse(localStorage.getItem('text'))
 
+vecchi()
+
+function vecchi() {
+
+  for (let i = 0; i < archiviati.length; i++) {
+    console.log(archiviati[i])
+  const message = document.createElement("div");
+  message.classList.add('message')
+  chat.appendChild(message)
+  let msgText = document.createElement('div')
+  msgText.innerHTML = archiviati[i]
+  message.appendChild(msgText)
+
+  let span = document.createElement('span')
+  span.classList.add('time')
+  message.appendChild(span)
+
+
+  let date = document.createElement('div')
+  date.classList.add('nowTime')
+  date.innerHTML = today.getHours() + ':' + today.getMinutes()
+
+  span.appendChild(date)
+
+  let icon = document.createElement("i");
+  icon.classList.add('fa')
+  icon.classList.add('fa-check-double')
+  icon.classList.add('read')
+  span.appendChild(icon)
+  
+ }
+}
 
 
